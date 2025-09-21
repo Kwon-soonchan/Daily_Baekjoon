80568d1178911dd5e1d54a4846f0164662aa079f
Kwon-soonchan <114489708+Kwon-soonchan@users.noreply.github.com>
2025-09-21 22:45:37 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 392 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+		long long dp[1000001];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+		}
+		cout << dp[n] % 1000000009 << endl;
+	}
+}
