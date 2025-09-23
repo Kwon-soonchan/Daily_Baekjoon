fe88fa3f389964329eedc66b6b9086849934e3c4
Kwon-soonchan <114489708+Kwon-soonchan@users.noreply.github.com>
2025-09-23 15:23:31 +0900

    [Silver IV] Title: 문자열 집합, Time: 316 ms, Memory: 7792 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	set<string> s;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		s.insert(str);
+	}
+
+	int cnt = 0;
+	for (int i = 0; i < m; i++) {
+		string str;
+		cin >> str;
+		if (s.find(str) != s.end()) {
+			cnt++;
+		}
+	}
+
+	cout << cnt << endl;
+}
