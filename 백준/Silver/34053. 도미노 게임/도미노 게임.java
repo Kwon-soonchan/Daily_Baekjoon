25f554cfacf00736c576e4a1c1849586f2ed439c
Kwon-soonchan <114489708+Kwon-soonchan@users.noreply.github.com>
2026-04-10 19:48:15 +0900

    [Silver III] Title: 도미노 게임, Time: 492 ms, Memory: 105536 KB -BaekjoonHub

---
+++ 백준/Silver/34053. 도미노 게임/도미노 게임.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int minNum = Integer.MAX_VALUE;
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                sum += num;
+
+                if (num < minNum) {
+                    minNum = num;
+                }
+            }
+        }
+
+        bw.write((sum - minNum) + "");
+        bw.flush();
+        bw.close();
+    }
+}
